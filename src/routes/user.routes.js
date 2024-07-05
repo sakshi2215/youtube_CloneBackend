cbf14c1fe6706669f2a4ab03a5587085fd244311
Sakshi <sakshi35sin@gmail.com>
2024-07-05 12:45:27 +0530

    feat: Add change password and update account endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router} from "express"
-import { logoutUser,loginUser,registerUser, refreshAcessToken, updateAvatar } from "../controllers/user.controller.js"
+import { logoutUser,loginUser,registerUser, refreshAcessToken, updateAvatar, changeCurrentPassword, getCurrentUser, updateAccountDetails } from "../controllers/user.controller.js"
 import {upload}from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
@@ -26,6 +26,18 @@ router.route("/logout").post(verifyJWT,logoutUser)
 
 router.route("/refresh-token").post(refreshAcessToken)
 
-router.route("/updateavatar").post(updateAvatar)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getuserChannelProfile)
+
+router.route("watch-history").get(verifyJWT, getWatchHistory)
 
 export default router
