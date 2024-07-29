2152d200133e2f0bbcac415d27bf709305032ce8
Sakshi <sakshi35sin@gmail.com>
2024-07-29 16:08:06 +0530

    User route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -36,15 +36,15 @@ router.route("/refresh-token").post(refreshAccessToken)
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
 
-router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar)
-
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 
-router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
 
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
-router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
 
 router.route("/channel-profile").get(verifyJWT, getuserChannelProfile)
 
