19c08de8bbefb6313bb4969e98c5a9027e76c437
Sakshi <sakshi35sin@gmail.com>
2024-07-31 21:52:48 +0530

    feat: Added createTweet functionality

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -7,6 +7,43 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const {content} = req.body;
+    if(!content){
+        throw new ApiError(400, "Invalid Tweet");
+    }
+    if(content.trim().length===0){
+        throw new ApiError(400, "Tweet cannot be Empty");
+    }
+    const owner = req.user?._id
+    if(!isValidObjectId(owner)){
+        throw new ApiError(400, "Invalid user")
+    }
+
+    const tweet = await Tweet.create(
+        {
+            content:content,
+            owner : owner
+        }
+    );
+
+    if(!tweet){
+        throw new ApiError(500, "Error while creating the Tweet");
+    }
+    const tweetData = {
+        ...tweet._doc,
+        owner:{
+            fullname: req.user?.fullname,
+            username : req.user?.username,
+            avatar: req.user?.avatar,
+        },
+        likes:0,
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,"Successfully created the Tweet")
+    );
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
