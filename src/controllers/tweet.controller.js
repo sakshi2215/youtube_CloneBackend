a18b8e7c66569b5bbefa296928e0f8a1ac2109bf
Sakshi <sakshi35sin@gmail.com>
2024-07-31 22:13:06 +0530

    Feat: added update and delete Tweet Functionality

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -47,15 +47,63 @@ const createTweet = asyncHandler(async (req, res) => {
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
+    // TODO: get user tweets  
 })
 
+//TODO Done: update tweet
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    
+    const{tweetId} = req.params;
+    const {content } = req.body;
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweet Id");
+    }
+
+    if(!content){
+        throw new ApiError(400, "Content must be Provided");
+    }
+    if(content.trim().length ===0){
+        throw new ApiError(400, "Content must be Provided");
+    }
+
+    const tweetUpdate = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            content: content
+        }
+    )
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, tweetUpdate, "Successfully Updated the tweet")
+    );
 })
 
+
+//TODO Done: delete tweet
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    const{tweetid} = req.params;
+
+    if(!isValidObjectId(tweetid)){
+        throw new ApiError(400, "Tweet Id is Invalid");
+    }
+
+    const deleteTweet = await Tweet.findAndDelete(tweetid);
+
+    if(!deleteTweet){
+        throw new ApiError(500, "Error while deteing the Tweet");
+    }
+
+    const deleteTweetLike = await Like.deleteMany({
+        tweet: mongoose.Types.ObjectId(tweetid)
+    });
+    
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, "Successfully Deleted the Tweet")
+    );
 })
 
 export {
